8e626ee2ae0872ffa7f51d0766feacaaba1cba37
Thomas Fischer <thomas.fischer@ufz.de>
2021-07-30 07:03:19 +0200

    Create serial port using '/dev/ttyUSB0' instead of '/dev/ttyACM0'.

--- test/arduino/main.cpp
+++ test/arduino/main.cpp
@@ -3,11 +3,10 @@
 using namespace mn::CppLinuxSerial;
 
 int main() {
-	// Create serial port object and open serial port
-    SerialPort serialPort("/dev/ttyACM0", BaudRate::B_9600);
-	// SerialPort serialPort("/dev/ttyACM0", 13000);
-    serialPort.SetTimeout(-1); // Block when reading until any data is received
-	serialPort.Open();
+  // Create serial port object and open serial port
+  SerialPort serialPort("/dev/ttyUSB0", BaudRate::B_9600);
+  serialPort.SetTimeout(-1); // Block when reading until any data is received
+  serialPort.Open();
 
 	// Write some ASCII datae
 	// serialPort0.Write("Hello");
