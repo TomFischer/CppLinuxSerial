cca973d71e12593fb3c9cbdb12f24c2a97bf278b
Thomas Fischer <thomas.fischer@ufz.de>
2021-07-30 07:03:50 +0200

    Format test/arduino/main.cpp.

--- test/arduino/main.cpp
+++ test/arduino/main.cpp
@@ -8,16 +8,16 @@ int main() {
   serialPort.SetTimeout(-1); // Block when reading until any data is received
   serialPort.Open();
 
-	// Write some ASCII datae
-	// serialPort0.Write("Hello");
+  // Write some ASCII datae
+  // serialPort0.Write("Hello");
 
-	// Read some data back
-    while(1) {
-        std::string readData;
-        serialPort.Read(readData);
-        std::cout << "Received data: " << readData;
-    }
+  // Read some data back
+  while (1) {
+    std::string readData;
+    serialPort.Read(readData);
+    std::cout << readData;
+  }
 
-	// Close the serial port
-	serialPort.Close();
+  // Close the serial port
+  serialPort.Close();
 }
